b5b645301c20d2d3c36c18a3f48a46c7a41e6243
ashishstirling <ashishkulkarni@hotmail.com>
2022-09-05 15:51:57 +0100

    Demo17Model added

--- src/main/java/com/example/demo17/controllers/Demo17Controller.java
+++ src/main/java/com/example/demo17/controllers/Demo17Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo17.controllers;
 
+import com.example.demo17.models.Demo17Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Demo17Controller {
     @GetMapping("/hello")
-    public String getHello() {
-        return "Hello World";
+    public Demo17Model getHello() {
+        Demo17Model dm = new Demo17Model();
+        dm.setMessage("Hello World");
+        return dm;
     }
 }
---
+++ src/main/java/com/example/demo17/models/Demo17Model.java
@@ -0,0 +1,8 @@
+package com.example.demo17.models;
+
+import lombok.Data;
+
+@Data
+public class Demo17Model {
+    private String message;
+}
